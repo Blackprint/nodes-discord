48b1b7ce9dd1f34add1c9fc2a38464e6b69ffab0
StefansArya <stefansarya1@gmail.com>
2022-08-25 15:32:43 +0700

    Update configurations

--- blackprint.config.js
+++ blackprint.config.js
@@ -1,28 +1,31 @@
 module.exports = {
 	// Your unique package name, will be the prefix on the Blackprint editor
-	name: "Empty Template",
-	description: "Your awesome nodes",
+	name: "Discord",
+	description: "Blackprint nodes for Discord.js",
 
 	// If this set to true, the compiler will skip this config/module
-	disabled: true,
+	disabled: false,
 
 	// Uncomment this if you want to copy /dist/... files to current folder
 	// Especially if you want to publish it to NPM Registry
-	// hardlinkTo: "./dist",
+	hardlinkTo: "./dist",
 
 	// Unique prefix for .html or .sf template
 	// Please use format 'BPIC/LibraryName'
-	templatePrefix: "BPIC/LibraryName",
+	templatePrefix: "BPIC/Discord",
 
 	// Optional: Add header to every combined file
-	header: "/* Your Nodes \n MIT Licensed */",
+	header: "/* MIT Licensed */",
+
+	// Optional: Extract registerNode's JSDocs
+	bpDocs: '@cwd/dist/nodes-discord.docs.json',
 
 	// (Required)
 	// The .js file probably can be imported for non-browser too
 	// Maybe you want to write Node.js compatible node on ".js"
 	// and browser compatible node on ".sf" file extension
 	js:{
-		file:'@cwd/dist/nodes-rename-me.mjs', // @cwd = directory where you start the Node.js
+		file:'@cwd/dist/nodes-discord.mjs', // @cwd = directory where you start the Node.js
 
 		// We're not using `await imports.task()` so let's just use `mjs`
 		wrapped: 'mjs', // Wrap the entire .js to .mjs
@@ -37,7 +40,7 @@ module.exports = {
 	// This extension can contain html, scss, and js
 	// But only use this if you only develop for browser API
 	sf:{
-		file:'@cwd/dist/nodes-rename-me.sf', // will have sf.css and sf.mjs
+		file:'@cwd/dist/nodes-discord.sf', // will have sf.css and sf.mjs
 
 		// Use `async-mjs` if we want to use `await imports.task()` to avoid waiting this module
 		wrapped: 'async-mjs', // Wrap the entire .js in async IIFE to .mjs file
